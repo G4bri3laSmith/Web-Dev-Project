6f5cc413f0b66277cd500935ef7896b9afef9c95
G4bri3laSmith <gabby.smith22@bathspa.ac.uk>
2023-05-18 11:20:44 +0100

    Addition of code comments

--- Web-Dev-Project.js
+++ Web-Dev-Project.js
@@ -1,5 +1,7 @@
+//Outputs the following message to the web console if js file is correctly linked
 console.log("js file is linked correctly")
 
+//Function implemented into html button which allows user to press it and be taken from the bottom to the top of the page
 function toTop() {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
 }
